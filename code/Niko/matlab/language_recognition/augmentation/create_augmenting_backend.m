2d49e8e7520bfa170a03d808aac421d0ce4bafc3
bsxfan <bsxfan@gmail.com>
2017-10-12 10:07:59 +0200

    language rec code refactored and expanded

---
+++ code/Niko/matlab/language_recognition/augmentation/create_augmenting_backend.m
@@ -0,0 +1,18 @@
+function ABE = create_augmenting_backend(nu,dim,T,K,L)
+% Inputs:
+%   nu: scalar nu>0, t-distribution degrees of freedom
+%   dim: ivector dimension
+%   T: i-vector extr\zctor T-matrix
+%   K: UBM size
+%   L: number of languages
+
+    fdim = size(T,1)/K;
+    assert(dim==size(T,2));
+    
+    
+
+
+
+
+
+end
